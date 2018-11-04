42503bbf2296e0bbb48c140640d631bc960ef750
0x6368656174 <0x6368656174@gmail.com>
2018-11-05 08:28:08 +1000

    feat: Add siema slider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { default as Siema } from 'siema';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'siema-test';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    new Siema();
+  }
 }
